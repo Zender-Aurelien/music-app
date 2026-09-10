0ab31cfa82251578fa45dd4a2acfe206f123dc9b
Zender-Aurelien <zenderaurelien4@gmail.com>
2026-09-10 01:24:28 -0400

    testing requests

--- jamendo-api/app.py
+++ jamendo-api/app.py
@@ -1,20 +1,29 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, jsonify, request
+import requests
 
-BASE_URL="https://api.jamendo.com"
+BASE_URL="https://api.jamendo.com/v3.0/tracks/"
 
 load_dotenv()
 client_id=os.getenv("CLIENT_ID")
 
-
 app = Flask(__name__)
 
-@app.route("/")
-def get_tracks():
+def get_tracks(fuzzy_tags="rock", limit=5):
   params={'client_id':client_id,
           'format':'json',
+          'limit':limit,
+          'fuzzytags':fuzzy_tags,
+          'include':'musicinfo'
           }
+  response=requests.get(BASE_URL, params=params)
+  data=response.json()
+  return jsonify(data.get('results', []))
+  
+@app.route("/")
+def index():
+  return get_tracks()
 
 if __name__ in "__main__":
   app.run(debug=True)
